a0a8d93528e81a2261f7c26e797346f8e1355140
faysal505 <127836350+faysal505@users.noreply.github.com>
2023-04-12 14:51:42 +0600

    Update ch.py

--- ch.py
+++ ch.py
@@ -2,16 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
-with open("data.txt", 'w') as f:
-    f.write("jsonns")
+
 
 @app.route('/')
 def home():
+    with open("data.txt", 'w') as f:
+        f.write("jsonns")
+    return
+@app.route("/home")
+def n():
     with open("data.txt", 'r') as f:
         d = f.read()
     return d
 
-app.run()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
