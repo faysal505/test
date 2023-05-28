23b7137d747e429fffefa60a2e3c6f2c27902b8d
faysal505 <127836350+faysal505@users.noreply.github.com>
2023-05-28 16:57:16 +0600

    Update ch.py

--- ch.py
+++ ch.py
@@ -1,19 +1,40 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
+db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://mydata_839h_user:J56VGKBunMhYrRrPjdJxTrcnYD55bdDl@dpg-cgts3nt269vbmevj9tfg-a.oregon-postgres.render.com/mydata_839h"
+db.init_app(app)
+
+class Nid(db.Model):
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    nid = db.Column(db.String(50), nullable=False)
+    birth = db.Column(db.String(50), nullable=False)
 
 
 
-@app.route('/')
-def home():
-    return "Welcome Page"
-@app.route("/home")
-def n():
-    with open("data.txt", 'r') as f:
-        d = f.read()
-    return d
+with app.app_context():
+    db.create_all()
 
+
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = Nid.query.all()  # Retrieve all records from the User table
+    user_list = []
+    for user in users:
+        user_data = {
+            'name': user.name,
+            'nid': user.nid,
+            'birth': user.birth
+        }
+        user_list.append(user_data)
+    return jsonify(user_list)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
